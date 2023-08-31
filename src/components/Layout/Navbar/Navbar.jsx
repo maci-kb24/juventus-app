2795bb1f208811055fff6bc5a2197e3312eb63e4
Marjan Stojanovski <macikb8@yahoo.com>
2023-08-31 21:31:18 +0200

    added assets

--- src/components/Layout/Navbar/Navbar.jsx
+++ src/components/Layout/Navbar/Navbar.jsx
@@ -1,7 +1,21 @@
+import Logo from "../../../assets/images/logos/juve_logo.png";
+
 const Navbar = () => {
   return (
     <header className="header">
-      <h1>Header</h1>
+      <nav>
+        <div>
+          <a>
+            <img src={Logo} alt={Logo} height={64} width={64} />
+          </a>
+        </div>
+        <div>
+          <ul>
+            <li>THE TEAM</li>
+            <li>MATCHES</li>
+          </ul>
+        </div>
+      </nav>
     </header>
   );
 };
