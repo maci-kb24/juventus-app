2b4a1ee56bf786ab78fd3938551b0a34e38cfd5c
Marjan Stojanovski <macikb8@yahoo.com>
2023-09-03 20:32:50 +0200

    fix footer component

--- src/components/Layout/Footer/Footer.jsx
+++ src/components/Layout/Footer/Footer.jsx
@@ -1,13 +1,39 @@
 import Logo from "../../../assets/images/logos/juve_logo.png";
 import { Box, Typography } from "@mui/material";
+import BottomNavigation from "@mui/material/BottomNavigation";
+// import BottomNavigationAction from "@mui/material/BottomNavigationAction";
+// import RestoreIcon from "@mui/icons-material/Restore";
+// import FavoriteIcon from "@mui/icons-material/Favorite";
+// import LocationOnIcon from "@mui/icons-material/LocationOn";
+// import { useState } from "react";
 
 const Footer = () => {
   return (
-    <Box sx={{ py: 2, textAlign: "center" }} className="footer">
-      <img src={Logo} alt={Logo} width={40} />
-      <Typography mt={1}>
-        Juventus Football Club 2023. All Rights Reserved.
-      </Typography>
+    <Box>
+      {/* <Box sx={{ py: 2, textAlign: "center" }} className="footer">
+        <img src={Logo} alt={Logo} width={40} />
+        <Typography mt={1}>
+          Juventus Football Club 2023. All Rights Reserved.
+        </Typography>
+      </Box> */}
+      <BottomNavigation
+        sx={{
+          backgroundColor: "#212121",
+          py: 2,
+          height: "auto",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        {/* <BottomNavigationAction label="Recents" icon={<RestoreIcon />} />
+        <BottomNavigationAction label="Favorites" icon={<FavoriteIcon />} />
+        <BottomNavigationAction label="Nearby" icon={<LocationOnIcon />} /> */}
+
+        <img src={Logo} alt={Logo} width={40} />
+        <Typography mt={1}>
+          Juventus Football Club 2023. All Rights Reserved.
+        </Typography>
+      </BottomNavigation>
     </Box>
   );
 };
--- src/components/Layout/Navbar/Navbar.jsx
+++ src/components/Layout/Navbar/Navbar.jsx
@@ -1,68 +1,29 @@
 import Logo from "../../../assets/images/logos/juve_logo.png";
-import {
-  Box,
-  List,
-  ListItem,
-  ListItemText,
-  AppBar,
-  Toolbar,
-} from "@mui/material";
+import { Box, AppBar, Toolbar, Button } from "@mui/material";
 import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <>
-      <AppBar position="static">
-        <Toolbar>
-          <Box style={{ flexGrow: 1 }}>
-            <Link to="/">
-              <img src={Logo} alt="Logo" width={40} />
-            </Link>
-          </Box>
-          <Box>
-            {/* <Button color="inherit" component={Link} to="/">
-              Home
-            </Button>
-            <Button color="inherit" component={Link} to="/team">
-              The Team
-            </Button>
-            <Button color="inherit" component={Link} to="/matches">
-              Matches
-            </Button> */}
-            <List>
-              <ListItem>
-                <Link to="/">
-                  <ListItemText primary="Home" />
-                </Link>
-              </ListItem>
-              <ListItem>
-                <Link to="/team">
-                  <ListItemText primary="The Team" />
-                </Link>
-              </ListItem>
-              <ListItem>
-                <Link to="/matches">
-                  <ListItemText
-                    primary="Matches"
-                    sx={{ color: "text.primary" }}
-                  />
-                </Link>
-              </ListItem>
-            </List>
-          </Box>
-        </Toolbar>
-      </AppBar>
-      {/* <Box className="header" display={"flex"}>
-        <nav>
-          <Box>
-            <Link to="/">
-              <img src={Logo} alt={Logo} width={40} />
-            </Link>
-          </Box>
-          <Box></Box>
-        </nav>
-      </Box> */}
-    </>
+    <AppBar position="static">
+      <Toolbar sx={{ justifyContent: "space-between", py: 2 }}>
+        <Box>
+          <Link to="/">
+            <img src={Logo} alt="Logo" width={40} />
+          </Link>
+        </Box>
+        <Box>
+          <Button color="inherit" component={Link} to="/">
+            Home
+          </Button>
+          <Button color="inherit" component={Link} to="/team">
+            The Team
+          </Button>
+          <Button color="inherit" component={Link} to="/matches">
+            Matches
+          </Button>
+        </Box>
+      </Toolbar>
+    </AppBar>
   );
 };
 
