5022ade64bc12e2ef289bf80e16c6f09e4394941
Marjan Stojanovski <macikb8@yahoo.com>
2023-09-05 16:48:31 +0200

    add jumbotron content & styling

--- src/components/Jumbotron/Jumbotron.jsx
+++ src/components/Jumbotron/Jumbotron.jsx
@@ -1,9 +1,53 @@
-import { Box } from "@mui/material";
+import { Box, Stack, Typography } from "@mui/material";
+import featured from "../../assets/images/featured.png";
 
 const Jumbotron = () => {
   return (
-    <Box className="jumbotron">
-      <h3>Jumbotron</h3>
+    <Box
+      component="section"
+      className="jumbotron"
+      sx={{ backgroundColor: "grey.main", minHeight: "100vh" }}
+    >
+      <Stack
+        direction="row"
+        justifyContent={"center"}
+        alignItems={"center"}
+        position={"relative"}
+      >
+        <Box position={"absolute"} left={"22%"} bottom={"-18%"}>
+          <Typography sx={{ lineHeight: "3" }}>
+            <Typography
+              variant="h1"
+              component="span"
+              sx={{ backgroundColor: "error.main" }}
+            >
+              38
+            </Typography>
+            <br />{" "}
+            <Typography
+              variant="h3"
+              component="span"
+              sx={{
+                backgroundColor: "error.main",
+                textTransform: "uppercase",
+              }}
+            >
+              seria a
+            </Typography>
+            <br />
+            <Typography
+              variant="h3"
+              component="span"
+              sx={{ backgroundColor: "error.main", textTransform: "uppercase" }}
+            >
+              championships
+            </Typography>
+          </Typography>
+        </Box>
+        <Box>
+          <img src={featured} alt={featured} />
+        </Box>
+      </Stack>
     </Box>
   );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -5,9 +5,40 @@ import "normalize.css";
 import "./index.css";
 import "@fontsource/righteous";
 import "@fontsource/righteous/400.css";
+import { ThemeProvider, createTheme } from "@mui/material";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#212121",
+    },
+    error: {
+      main: "#EF484D",
+    },
+    grey: {
+      main: "#CCCCCC",
+      light: "#E0E0E0",
+      lighter: "#EDEDED",
+    },
+    text: {
+      primary: "#FFFFFF",
+      secondary: "#333333",
+    },
+    background: {
+      default: "#333333",
+      error: "#EF484D",
+      dark: "#212121",
+    },
+  },
+  typography: {
+    fontFamily: "Righteous",
+  },
+});
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
